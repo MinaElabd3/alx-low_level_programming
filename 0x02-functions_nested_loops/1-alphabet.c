8d231abd54b68c58db7a72f28f962009a8e8769c
MinaElabd3 <minaelabd3@gmail.com>
2023-06-20 07:53:17 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+* main- print alphabet in lowercase
+* return: 0 (success)
+*/
+
+
+	void print_alphabet(void)
+{
+	char letter;
+
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
+
+
+	_putchar('\n');
+}
+
