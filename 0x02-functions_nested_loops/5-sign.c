70a4f0c0fb4080e5130cc9c552063629eacf5c2d
MinaElabd3 <minaelabd3@gmail.com>
2023-06-20 14:32:09 -0700

    N

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of number
+ *
+ * @n: number
+ *
+ * Return: 1 (is greater than zero) or 0 (zero)
+ * or -1 (is less than zero)
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
