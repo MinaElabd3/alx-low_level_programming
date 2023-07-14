eae7d700d1cbbdbe1ed5aa8ea082468ddc7e3cbc
MinaElabd3 <minaelabd3@gmail.com>
2023-07-14 15:59:10 -0700

    N

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,35 +12,29 @@
  */
 
 
-char *string_nconcat(char *s1, char *s2, unsigned int n) {
-  // Check if either string is NULL.
-  if (s1 == NULL) {
-    s1 = "";
-  }
-  if (s2 == NULL) {
-    s2 = "";
-  }
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+size_t s1_len = strlen(s1);
+if (n >= strlen(s2))
+{
+n = strlen(s2);
+}
+char *new_string = malloc(s1_len + n + 1);
 
-  // Calculate the length of the first string.
-  size_t s1_len = strlen(s1);
+memcpy(new_string, s1, s1_len);
 
-  // Check if n is greater than or equal to the length of s2.
-  if (n >= strlen(s2)) {
-    n = strlen(s2);
-  }
+memcpy(new_string + s1_len, s2, n);
 
-  // Allocate memory for the new string.
-  char *new_string = malloc(s1_len + n + 1);
+new_string[s1_len + n] = '\0';
 
-  // Copy the first string into the new string.
-  memcpy(new_string, s1, s1_len);
-
-  // Copy the first n bytes of the second string into the new string.
-  memcpy(new_string + s1_len, s2, n);
-
-  // Terminate the new string.
-  new_string[s1_len + n] = '\0';
-
-  return new_string;
+return (new_string);
 }
 
