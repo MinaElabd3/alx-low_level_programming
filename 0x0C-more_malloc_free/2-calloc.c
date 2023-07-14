6f7a0224c1e8182f8768c83f9fdbddc8c8d2089a
MinaElabd3 <minaelabd3@gmail.com>
2023-07-14 16:13:27 -0700

    N

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+
+	void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+	ptr[i] = 0;
+
+	return (ptr);
+
+}
