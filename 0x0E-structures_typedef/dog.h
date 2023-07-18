a5e833acb51e437b6a666162dde4bfa02848969c
MinaElabd3 <minaelabd3@gmail.com>
2023-07-17 19:21:44 -0700

    N

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's info
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
