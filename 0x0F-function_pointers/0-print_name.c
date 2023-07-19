898735847acc8e3a220c2c2cc7c891eadca3cfe0
MinaElabd3 <minaelabd3@gmail.com>
2023-07-19 14:18:08 -0700

    N

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - a function that prints a name.
@@ -8,8 +9,11 @@
  **/
 
 void print_name(char *name, void (*f)(char *))
-
 {
-	(*f)(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
+
 
